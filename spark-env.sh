e26ad6e11aca94326f44c997dab4d7049eced979
orozcohsu <orozcohsu@hotmail.com>
2018-07-10 03:14:16 -0700

    update

--- spark-env.sh
+++ spark-env.sh
@@ -1,5 +1,5 @@
 export SPARK_DAEMON_JAVA_OPTS="-Dspark.deploy.recoveryMode=ZOOKEEPER -Dspark.deploy.zookeeper.url=master:2181,slaver1:2181,slaver2:2181 -Dspark.deploy.zookeeper.dir=/opt/sparkha"
-export SPARK_HISTORY_OPTS="-Dspark.history.retainedApplications=10   -Dspark.history.fs.logDirectory=hdfs://mycluster/historyserverforspark"
+export SPARK_HISTORY_OPTS="-Dspark.history.retainedApplications=3   -Dspark.history.fs.logDirectory=hdfs://mycluster/historyserverforspark"
 
 export SCALA_HOME=/usr/bin/scala
 export JAVA_HOME=/usr/java/java
